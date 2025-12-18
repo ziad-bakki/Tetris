59f1babf987488b6a2eec912516d815e7ad9bea3
ziad-bakki <bakkizia@msu.edu>
2025-12-18 15:06:53 +0300

    removed hard coded array slice

--- tetris-frontend/app/components/nextpieces.tsx
+++ tetris-frontend/app/components/nextpieces.tsx
@@ -2,18 +2,18 @@ import { GameObject, Piece } from "../interfaces/interfaces";
 import { PiecePreview } from "./piecepreview";
 
 interface NextPiecesProps {
-    game: GameObject;
+  game: GameObject;
 }
 export function NextPieces({ game }: NextPiecesProps) {
-    const nextPieces: Piece[] = game.nextPieces.slice(0, 4);
+  const nextPieces: Piece[] = game.nextPieces;
 
-    return (
-        <div className="flex flex-col gap-2">
-            {nextPieces.map((piece, index) => (
-                <div key={index} className="w-[6vw] h-[6vw] border">
-                    <PiecePreview piece={piece} />
-                </div>
-            ))}
+  return (
+    <div className="flex flex-col gap-2">
+      {nextPieces.map((piece, index) => (
+        <div key={index} className="w-[6vw] h-[6vw] border">
+          <PiecePreview piece={piece} />
         </div>
-    );
+      ))}
+    </div>
+  );
 }
