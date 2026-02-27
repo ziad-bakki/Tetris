a3129e0996fde82dba472aa29616c7609afa5fda
ziad-bakki <ziadbakki@outlook.com>
2026-02-27 13:01:24 -0500

    added deleteuser method from backend

--- tetris-frontend/app/context/api.ts
+++ tetris-frontend/app/context/api.ts
@@ -1,6 +1,7 @@
 import { supabase } from "@/lib/supabase";
 import axios from "axios";
 import { Profile } from "../interfaces/backendtypes";
+import { headers } from "next/headers";
 
 const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
@@ -18,6 +19,30 @@ export async function GetUserByID(): Promise<Profile> {
   const response = await axios.get(endpoint);
 
   return response.data
-
-
 }
+
+
+export async function DeleteUser() {
+  const { data } = await supabase.auth.getSession();
+  const accessToken = data.session?.access_token;
+  const id = data.session?.user.id;
+  const endpoint = `${BACKEND_URL}/users/${id}`;
+
+  // const response = await axios.delete(endpoint,);
+  const response = await fetch(
+    endpoint,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
+  )
+
+  if (response.ok) {
+    return response.json();
+  }
+
+  return { message: "Failed to delete user" };
+}
+
